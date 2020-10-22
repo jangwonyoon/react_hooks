0bb425754693614256640af9b3d8f492562620b1
TheJang <wkddnjs7310@naver.com>
2020-10-22 15:21:23 +0900

    useEffect

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import UseInput from './useInput';
 import UseTabs from './useTabs';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <UseTabs />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
